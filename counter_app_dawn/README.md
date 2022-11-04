4a2279da0c010a3bb95a8e5fa95e78a8c16b3282
Łukasz Huculak <ukasz123@gmail.com>
2022-11-04 07:15:25 +0100

    Initial Dawn app

---
+++ counter_app_dawn/README.md
@@ -0,0 +1,21 @@
+<img
+    src="https://raw.githubusercontent.com/Hawmex/dawn/main/example/web/assets/banner.svg"
+    width="100%"
+/>
+
+---
+
+# counter_dawn
+
+A Dawn App.
+
+[ [Visit Dawn's official website →](https://dawn-dev.netlify.app) ]
+
+## Serve Your App
+
+```
+webdev serve
+```
+
+[ [Read more →](https://dawn-dev.netlify.app) ]
+
---
+++ counter_app_dawn/web/main.dart
@@ -0,0 +1,71 @@
+import 'package:dawn/dawn.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return const Container(
+      [
+        Image(
+          '/assets/logo.svg',
+          style: Style({'width': '128px', 'height': '128px'}),
+          animation: Animation(
+            keyframes: [
+              Keyframe(
+                offset: 0,
+                style: Style({'transform': 'translateY(0px)'}),
+              ),
+              Keyframe(
+                offset: 1,
+                style: Style({'transform': 'translateY(8px)'}),
+              ),
+            ],
+            duration: Duration(seconds: 1),
+            easing: Easing(0.2, 0, 0.4, 1),
+            direction: AnimationDirection.alternate,
+            iterations: double.infinity,
+          ),
+        ),
+        Text(
+          'Welcome to Dawn',
+          style: Style({
+            'font-size': '24px',
+            'font-weight': 'bold',
+            'color': '#00e690',
+          }),
+        ),
+        Container([
+          Text('To get started, edit '),
+          Text(
+            'web/main.dart',
+            style: Style({
+              'font-family': 'monospace',
+              'background': '#eeeeee',
+              'border-radius': '4px',
+              'padding': '4px',
+            }),
+          ),
+          Text(' and save to reload.'),
+        ]),
+      ],
+      style: Style({
+        'display': 'flex',
+        'flex-flow': 'column',
+        'justify-content': 'center',
+        'text-align': 'center',
+        'align-items': 'center',
+        'gap': '16px',
+        'padding': '16px',
+        'width': '100%',
+        'min-height': '100vh',
+        'background': '#ffffff',
+        'color': '#000000',
+        'font-family': '"Jost", system-ui',
+        'user-select': 'none',
+      }),
+    );
+  }
+}
